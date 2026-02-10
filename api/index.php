3f2e2c87ba82fc549623c7431752250a1ca0104a
aryanibos <aryaisnaidi01@gmail.com>
2026-02-10 14:51:49 +0700

    Updated Deploy

--- api/index.php
+++ api/index.php
@@ -1,7 +1,9 @@
 <?php
 
 // Suppress Deprecation Warnings (Laravel 5.8 on PHP 8.x compatibility)
-error_reporting(E_ALL ^ E_DEPRECATED);
+error_reporting(E_ALL & ~E_DEPRECATED & ~E_STRICT);
+ini_set('display_errors', '0');
+ini_set('display_startup_errors', '0');
 
 // Forward Vercel requests to Laravel entry point
 require __DIR__ . '/../public/index.php';
